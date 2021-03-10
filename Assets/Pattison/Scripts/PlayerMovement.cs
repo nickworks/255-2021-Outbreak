60d96c9fb41eecaa12e4d846f1d807411ba7314f
Nick Pattison <pattison.nick@gmail.com>
2021-03-10 16:24:09 -0500

    PlayerMovement has a FSM, and player can now aim at mouse

--- Assets/Pattison/Scripts/PlayerMovement.cs
+++ Assets/Pattison/Scripts/PlayerMovement.cs
@@ -6,9 +6,22 @@ using UnityEngine;
 namespace Pattison {
     public class PlayerMovement : MonoBehaviour {
 
+
+        public enum MoveState {
+            Regular, // 0
+            Dashing, // 1
+            Sprinting, // 2
+            Sneaking // 3
+        }
+
+
         public float playerSpeed = 10;
 
         private CharacterController pawn;
+
+        MoveState currentMoveState = MoveState.Regular;
+
+
 
         void Start() {
             pawn = GetComponent<CharacterController>();
@@ -16,14 +29,60 @@ namespace Pattison {
 
         // Update is called once per frame
         void Update() {
+
+
+            switch (currentMoveState) {
+                case MoveState.Regular:
+                    
+                    // do behavior for this state:
+                    
+                    MoveThePlayer(1);
+
+                    // transitions to other states:
+                    if (Input.GetButton("Fire1")) currentMoveState = MoveState.Sneaking;
+                    if (Input.GetButton("Fire3")) currentMoveState = MoveState.Sprinting;
+
+                    break;
+                case MoveState.Dashing:
+
+                    // do behavior for this state:
+
+                    // transitions to other states:
+
+                    break;
+                case MoveState.Sprinting:
+
+                    // do behavior for this state:
+
+                    MoveThePlayer(2);
+
+                    // transitions to other states:
+                    if (!Input.GetButton("Fire3")) currentMoveState = MoveState.Regular;
+                    if (Input.GetButton("Fire1")) currentMoveState = MoveState.Sneaking;
+
+                    break;
+                case MoveState.Sneaking:
+
+                    // do behavior for this state:
+
+                    MoveThePlayer(0.5f);
+
+                    // transitions to other states:
+                    if (!Input.GetButton("Fire1")) currentMoveState = MoveState.Regular;
+
+                    break;
+            }
+
+        }
+
+        private void MoveThePlayer(float mult = 1) {
             float h = Input.GetAxis("Horizontal");
             float v = Input.GetAxis("Vertical");
 
-
             Vector3 move = Vector3.right * h + Vector3.forward * v;
-            if(move.sqrMagnitude > 1) move.Normalize(); // fix bug with diagonal input vectors
+            if (move.sqrMagnitude > 1) move.Normalize(); // fix bug with diagonal input vectors
 
-            pawn.Move(move * Time.deltaTime * playerSpeed);
+            pawn.Move(move * Time.deltaTime * playerSpeed * mult);
         }
     }
 }
