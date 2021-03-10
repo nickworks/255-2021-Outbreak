7ae3c6e1deae357394ab7aebad6dfbbc3fd19691
Tom H <darksonic3211@gmail.com>
2021-03-10 16:25:56 -0500

    FSM in PlayerMovement and player can aim at mouse

--- Assets/Hodgkins/Scripts/PlayerAiming.cs
+++ Assets/Hodgkins/Scripts/PlayerAiming.cs
@@ -26,6 +26,15 @@ public class PlayerAiming : MonoBehaviour
             // find where the ray hit the plane
             Vector3 hitPos = ray.GetPoint(dis);
             if(debugObject) debugObject.position = hitPos;
+
+            Vector3 vectorToHitPos = hitPos - transform.position;
+
+            float angle = Mathf.Atan2(vectorToHitPos.x, vectorToHitPos.z);
+
+            angle /= Mathf.PI; // convert from 'radians' to 'half-circles'
+            angle *= 180; // convert from 'half-circles' to 'degrees'
+
+            transform.eulerAngles = new Vector3(0, angle, 0);
         }
 
     }
--- Assets/Hodgkins/Scripts/PlayerMovement.cs
+++ Assets/Hodgkins/Scripts/PlayerMovement.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 namespace Hodgkins {
     public class PlayerMovement : MonoBehaviour
     {
+        public enum MoveState
+        {
+            Regualar, // 0
+            Dashing, // 1
+            Sprinting, // 2
+            Sneaking // 3
+        }
+        
+        
+        
         public float playerSpeed = 10;
 
         private CharacterController pawn;
+
+        MoveState currentMoveState = MoveState.Regualar;
 
         void Start()
         {
@@ -17,6 +29,39 @@ namespace Hodgkins {
         // Update is called once per frame
         void Update()
         {
+            switch (currentMoveState)
+            {
+                case MoveState.Regualar:
+                    // do behaviour for this state
+                    MoveThePlayer(1);
+                    // transition to other state
+                    if (Input.GetButton("Fire3")) currentMoveState = MoveState.Sprinting;
+                    if (Input.GetButton("Fire1")) currentMoveState = MoveState.Sneaking;
+                    break;
+                case MoveState.Dashing:
+                    break;
+                case MoveState.Sprinting:
+                    // do behaviour for this state
+                    MoveThePlayer(2);
+                    // transition to other state
+                    if (!Input.GetButton("Fire3")) currentMoveState = MoveState.Regualar;
+                    break;
+                case MoveState.Sneaking:
+                    // do behaviour for this state
+                    MoveThePlayer(0.5f);
+                    // transition to other state
+                    if (!Input.GetButton("Fire1")) currentMoveState = MoveState.Regualar;
+                    if (Input.GetButton("Fire3")) currentMoveState = MoveState.Sprinting;
+
+                    break;
+            }
+
+
+
+        }
+
+        private void MoveThePlayer(float mult = 1)
+        {
             float h = Input.GetAxis("Horizontal");
             float v = Input.GetAxis("Vertical");
 
@@ -25,8 +70,7 @@ namespace Hodgkins {
 
             if (move.sqrMagnitude > 1) move.Normalize(); // fix diagonal movement bug
 
-            pawn.Move(move * Time.deltaTime * playerSpeed);
-
+            pawn.Move(move * Time.deltaTime * playerSpeed * mult);
         }
     }
 }
