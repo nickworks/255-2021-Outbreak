9a3ffc91b3792f9f2687a4160b4c30eccd10058d
Samuel <skepticalnarcissist@gmail.com>
2021-03-17 13:35:21 -0400

    Added dashing to PlayerMovement

--- Assets/Geib/Scripts/PlayerMovement.cs
+++ Assets/Geib/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -16,7 +17,8 @@ namespace Geib
             Regular, // 0
             Dashing, // 1
             Sprinting, // 2
-            Sneaking // 3
+            Sneaking, // 3
+            Shielding
         }
         
         
@@ -28,9 +30,22 @@ namespace Geib
         /// pawn is the reference to our character controller.
         /// </summary>
         private CharacterController pawn;
-
+        /// <summary>
+        /// This variable stores the current move state.
+        /// </summary>
         MoveState currentMoveState = MoveState.Regular;
-
+        /// <summary>
+        /// This variable holds the direction the player was facing when they initiated the dash action.
+        /// </summary>
+        private Vector3 dashDirection;
+        /// <summary>
+        /// How long the dash should take, in seconds:
+        /// </summary>
+        public float dashDuration = 0.25f;
+        /// <summary>
+        /// This stores how many seconds are left:
+        /// </summary>
+        public float dashTimer = 0;
         /// <summary>
         /// Runs on the first tick of the game.
         /// </summary>
@@ -45,6 +60,7 @@ namespace Geib
         void Update()
         {
 
+            Debug.Log(currentMoveState);
             switch (currentMoveState)
             {
                 case MoveState.Regular:
@@ -56,16 +72,27 @@ namespace Geib
                     // transition to other states:
                     if (Input.GetButton("Fire3")) currentMoveState = MoveState.Sprinting;
                     if (Input.GetButton("Fire1")) currentMoveState = MoveState.Sneaking;
+                    if (Input.GetButton("Fire2")) // Transition into Dashing
+                    {
+                        currentMoveState = MoveState.Dashing;
+                        float h = Input.GetAxis("Horizontal");
+                        float v = Input.GetAxis("Vertical");
+                        dashDirection = new Vector3(h, 0, v); // Vertical axis is on the Z-axis because the game is two dimensional and top-down
+                        // Clamps the length of dashDir to 1:
+                        if (dashDirection.sqrMagnitude > 1) dashDirection.Normalize();
+                    }
 
 
                     break;
                 case MoveState.Dashing:
 
                     // Do behavior for this state:
+                    DashThePlayer();
 
-                    
+                    dashTimer -= Time.deltaTime;
 
                     // transition to other states:
+                    if (dashTimer <= 0 ) currentMoveState = MoveState.Regular;
 
                     break;
                 case MoveState.Sprinting:
@@ -89,13 +116,20 @@ namespace Geib
                     MoveThePlayer(0.5f);
 
                     // transition to other states:
-                    if (!Input.GetButton("Fire1")) currentMoveState = MoveState.Sneaking;
+                    if (!Input.GetButton("Fire1")) currentMoveState = MoveState.Regular;
 
 
                     break;
             }
 
+        }
 
+        /// <summary>
+        /// This function holds the logic for the player's dash state
+        /// </summary>
+        private void DashThePlayer()
+        {
+            pawn.Move(dashDirection * Time.deltaTime * 100);
         }
 
         private void MoveThePlayer(float mult = 1)
