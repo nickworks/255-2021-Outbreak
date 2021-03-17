1958632ecc74b9f658f78432c415a5ccf3ea7af2
Zion <ekozmos25@gmail.com>
2021-03-17 13:36:13 -0400

    Added dashing to PlayerMovement
    + PlayerMovement.DashThePlayer() - this function moves the player while they are dashing

--- Assets/Miller/Scripts/PlayerMovement.cs
+++ Assets/Miller/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -18,13 +19,19 @@ namespace Miller
 
 
         public float playerSpeed = 10;
-
         private CharacterController pawn;
-
         MoveState currentMoveState = MoveState.Regular;
-        //1 = regular
-        //2 = dashing
-        //3 = sneaking
+
+        /// <summary>
+        /// How long a dash should take in seconds:
+        /// </summary>
+        public float dashDuration = 0.25f;
+
+        private Vector3 dashDirection;
+        /// <summary>
+        /// This stores how many seconds are left:
+        /// </summary>
+        private float dashTImer = 0;
 
         void Start()
         {
@@ -34,6 +41,8 @@ namespace Miller
         // Update is called once per frame
         void Update()
         {
+
+            print(currentMoveState);
 
             switch (currentMoveState)
             {
@@ -46,13 +55,26 @@ namespace Miller
                     // transitions to other states:
                     if (Input.GetButton("Fire1")) currentMoveState = MoveState.Sneaking;
                     if (Input.GetButton("Fire3")) currentMoveState = MoveState.Sprinting;
+                    if (Input.GetButton("Fire2"))
+                    {
+                        currentMoveState = MoveState.Dashing;
+                        float h = Input.GetAxis("Horizontal");
+                        float v = Input.GetAxis("Vertical");
+                        dashDirection = new Vector3(h, 0, v);
 
+                        //clamp the length of dashDir to 1:
+                        if (dashDirection.sqrMagnitude > 1) dashDirection.Normalize();
+                    }
                     break;
                 case MoveState.Dashing:
 
                     // do behavior for this state:
+                    DashThePlayer();
+
+                    dashTImer -= Time.deltaTime;
 
                     // transitions to other states:
+                    if (dashTImer <= 0) currentMoveState = MoveState.Regular;
 
                     break;
                 case MoveState.Sprinting:
@@ -80,6 +102,11 @@ namespace Miller
                     break;
             }
 
+        }
+
+        private void DashThePlayer()
+        {
+            pawn.Move(dashDirection * Time.deltaTime * 25);
         }
 
         private void MoveThePlayer(float mult = 1)
