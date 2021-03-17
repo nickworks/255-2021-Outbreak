f264860954776d316775c338f2bd497118cf3990
Rocco <roccojelso77@gmail.com>
2021-03-17 13:35:06 -0400

    In Class Update 3/17
    Added a Dash mechanic to the player movement

--- Assets/Jelsomeno/Scripts/PlayerMovement.cs
+++ Assets/Jelsomeno/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,16 +14,26 @@ namespace Jelsomeno
             Regular, //0
             Dashing, //1
             Sprinting, //2
-            Sneaking //3
+            Sneaking, //3
+            Shielding,
 
         }
 
 
         public float playerSpeed = 10;
-
         private CharacterController pawn;
-
         MoveState currentMoveState = MoveState.Regular;
+
+        /// <summary>
+        /// How long a dash should take, in seconds
+        /// </summary>
+        public float dashDuration = 0.25f;
+
+        private Vector3 dashDirection;
+        /// <summary>
+        /// This stores how many seconds are left
+        /// </summary>
+        private float dashTimer = 0;
 
         // Start is called before the first frame update
         void Start()
@@ -33,6 +44,8 @@ namespace Jelsomeno
         // Update is called once per frame
         void Update()
         {
+
+            print(currentMoveState);
 
             switch (currentMoveState)
             {
@@ -46,12 +59,30 @@ namespace Jelsomeno
                     if (Input.GetButton("Fire3")) currentMoveState = MoveState.Sprinting;
                     if (Input.GetButton("Fire1")) currentMoveState = MoveState.Sneaking;
 
+                    if (Input.GetButton("Fire2"))
+                    {
+
+                        currentMoveState = MoveState.Dashing;
+                        float h = Input.GetAxis("Horizontal");
+                        float v = Input.GetAxis("Veritcal");
+                        dashDirection = new Vector3(h, 0, v);
+                        // clamp length to dashDirection
+                        if (dashDirection.sqrMagnitude > 1) dashDirection.Normalize();
+
+                    }
+
                     break;
                 case MoveState.Dashing:
 
                     // do behavior for this state:
+                    DashThePlayer();
+
+                    dashTimer -= Time.deltaTime;
+
 
                     // transitions to other states:
+                    if (dashTimer <= 0) currentMoveState = MoveState.Regular;
+
 
                     break;
                 case MoveState.Sprinting:
@@ -76,6 +107,11 @@ namespace Jelsomeno
 
                     break;
             }
+        }
+
+        private void DashThePlayer()
+        {
+            pawn.Move(dashDirection * Time.deltaTime * 50);
         }
 
         private void MoveThePlayer(float mult = 1)
