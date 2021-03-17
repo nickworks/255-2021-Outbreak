ab7f528d26c662ce690670caf03f0398749793eb
Tom H <darksonic3211@gmail.com>
2021-03-17 16:33:42 -0400

    Started BasicEnemyController
    Also changed InputManager to add the reload function but that isn't being staged here for some reason.

---
+++ Assets/Hodgkins/Scripts/EnemyBasicController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hodgkins
+{
+    public class EnemyBasicController : MonoBehaviour
+    {
+        
+        static class States
+        {
+            public class State
+            {
+                protected EnemyBasicController enemy;
+
+                virtual public State Update()
+                {
+                    return null;
+                }
+                virtual public void OnStart(EnemyBasicController enemy)
+                {
+                    this.enemy = enemy;
+                }
+                virtual public void OnEnd()
+                {
+
+                }
+            }
+
+            public class Idle : State {  }
+            public class Pursuing : State {  }
+            public class Patrolling : State {  }
+            public class Stunned : State {  }
+            public class Death : State {  }
+            public class Attack1 : State {  }
+            public class Attack2 : State {  }
+            public class Attack3 : State {  }
+        }
+
+        private States.State state;
+
+
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            if (state == null) SwitchState(new States.Idle());
+
+            if(state != null) SwitchState(state.Update());
+
+
+
+        }
+
+        void SwitchState(States.State newState)
+        {
+            if (newState == null) return; // don't switch to nothing...
+
+            if(state != null) state.OnEnd(); // tell previous state it is done
+            state = newState; // swap states
+            state.OnStart(this);
+
+
+        }
+    }
+}
