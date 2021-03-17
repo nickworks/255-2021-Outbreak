12846c7d515153669b211a4c25e267f3ad2c8220
Nick Pattison <pattison.nick@gmail.com>
2021-03-17 13:34:54 -0400

    Added dashing to PlayerMovement
    + PlayerMovement.DashThePlayer() - this function moves the player while they are dashing.

--- Assets/Pattison/Scripts/PlayerMovement.cs
+++ Assets/Pattison/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,17 +12,25 @@ namespace Pattison {
             Regular, // 0
             Dashing, // 1
             Sprinting, // 2
-            Sneaking // 3
+            Sneaking, // 3
+            Shielding,
         }
 
 
         public float playerSpeed = 10;
-
         private CharacterController pawn;
-
         MoveState currentMoveState = MoveState.Regular;
 
+        /// <summary>
+        /// How long a dash should take, in seconds:
+        /// </summary>
+        public float dashDuration = 0.25f;
 
+        private Vector3 dashDirection;
+        /// <summary>
+        /// This stores how many seconds are left:
+        /// </summary>
+        private float dashTimer = 0;
 
         void Start() {
             pawn = GetComponent<CharacterController>();
@@ -30,6 +39,8 @@ namespace Pattison {
         // Update is called once per frame
         void Update() {
 
+
+            //print(currentMoveState);
 
             switch (currentMoveState) {
                 case MoveState.Regular:
@@ -42,12 +53,27 @@ namespace Pattison {
                     if (Input.GetButton("Fire1")) currentMoveState = MoveState.Sneaking;
                     if (Input.GetButton("Fire3")) currentMoveState = MoveState.Sprinting;
 
+                    if (Input.GetButton("Fire2")) { // transition to dashing
+
+                        currentMoveState = MoveState.Dashing;
+                        float h = Input.GetAxis("Horizontal");
+                        float v = Input.GetAxis("Vertical");
+                        dashDirection = new Vector3(h, 0, v);
+
+                        // clamp the length of dashDir to 1:
+                        if (dashDirection.sqrMagnitude > 1) dashDirection.Normalize();
+                    }
+
                     break;
                 case MoveState.Dashing:
 
                     // do behavior for this state:
+                    DashThePlayer();
+
+                    dashTimer -= Time.deltaTime;
 
                     // transitions to other states:
+                    if (dashTimer <= 0) currentMoveState = MoveState.Regular;
 
                     break;
                 case MoveState.Sprinting:
@@ -73,6 +99,12 @@ namespace Pattison {
                     break;
             }
 
+        }
+        /// <summary>
+        /// This function moves the player while they are dashing.
+        /// </summary>
+        private void DashThePlayer() {
+            pawn.Move(dashDirection * Time.deltaTime * 100);
         }
 
         private void MoveThePlayer(float mult = 1) {
