db83404293e31d9afb4685def0b54e554f4d53af
Nick Pattison <pattison.nick@gmail.com>
2021-03-17 13:48:39 -0400

    Moved my scripts to my namespace

--- Assets/Pattison/Scripts/CameraTracking.cs
+++ Assets/Pattison/Scripts/CameraTracking.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraTracking : MonoBehaviour
-{
+namespace Pattison {
+    public class CameraTracking : MonoBehaviour {
 
-    public Transform target;
+        public Transform target;
 
-    /// <summary>
-    /// Runs every time the physics engine ticks.
-    /// </summary>
-    void Update() {
-        if (target) {
-            transform.position = target.position;
+        /// <summary>
+        /// Runs every time the physics engine ticks.
+        /// </summary>
+        void Update() {
+            if (target) {
+                transform.position = target.position;
+            }
         }
     }
 }
--- Assets/Pattison/Scripts/PlayerAiming.cs
+++ Assets/Pattison/Scripts/PlayerAiming.cs
@@ -2,41 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAiming : MonoBehaviour {
+namespace Pattison {
+    public class PlayerAiming : MonoBehaviour {
 
-    private Camera cam;
+        private Camera cam;
 
-    public Transform debugObject;
+        public Transform debugObject;
 
-    void Start()
-    {
-        cam = Camera.main;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // make a ray and a plane:
-        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-        Plane plane = new Plane(Vector3.up, transform.position);
-
-        // does the ray hit the plane?
-        if(plane.Raycast(ray, out float dis)) {
-            
-            // find point where the ray hit the plane:
-            Vector3 hitPos = ray.GetPoint(dis);
-
-            if(debugObject) debugObject.position = hitPos;
-
-            Vector3 vectorToHitPos = hitPos - transform.position;
-
-            float angle = Mathf.Atan2(vectorToHitPos.x, vectorToHitPos.z);
-
-            angle /= Mathf.PI; // convert from "radians" to "half-circles"
-            angle *= 180; // convert from "half-circles" to "degrees"
-
-            transform.eulerAngles = new Vector3(0, angle, 0);
+        void Start() {
+            cam = Camera.main;
         }
 
+        // Update is called once per frame
+        void Update() {
+            // make a ray and a plane:
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            Plane plane = new Plane(Vector3.up, transform.position);
+
+            // does the ray hit the plane?
+            if (plane.Raycast(ray, out float dis)) {
+
+                // find point where the ray hit the plane:
+                Vector3 hitPos = ray.GetPoint(dis);
+
+                if (debugObject) debugObject.position = hitPos;
+
+                Vector3 vectorToHitPos = hitPos - transform.position;
+
+                float angle = Mathf.Atan2(vectorToHitPos.x, vectorToHitPos.z);
+
+                angle /= Mathf.PI; // convert from "radians" to "half-circles"
+                angle *= 180; // convert from "half-circles" to "degrees"
+
+                transform.eulerAngles = new Vector3(0, angle, 0);
+            }
+
+        }
     }
 }
