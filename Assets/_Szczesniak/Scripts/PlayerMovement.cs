ebeb9eedc38949dfcd8c645783eae59f0fbb6083
Gavin Szczesniak <szczesg@ferris.edu>
2021-03-17 13:37:51 -0400

    Added dashing mechanic in Lab
    - Stores amount of time of duration
    - Does amount of dash duration

--- Assets/_Szczesniak/Scripts/PlayerMovement.cs
+++ Assets/_Szczesniak/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,7 +10,8 @@ namespace Szczesniak {
             Regular, // 0
             Dashing, // 1
             Sprinting, // 2
-            Sneaking //3
+            Sneaking, //3
+            Shielding,
         }
 
         private Vector3 move = new Vector3();
@@ -19,8 +21,20 @@ namespace Szczesniak {
         private CharacterController pawn;
 
         private float dashTime = .3f;
-
         MoveState currentMoveState = MoveState.Regular;
+
+        /// <summary>
+        /// How long a dash should take, in seconds
+        /// </summary>
+        public float dashDuration = 0.25f;
+
+        private Vector3 dashDirection;
+
+        /// <summary>
+        /// This stores how many seconds are left:
+        /// </summary>
+        private float dashTimer = 0;
+
         // 1 = regular
         // 2 = dashing 
         // 3 = sneaking
@@ -32,6 +46,8 @@ namespace Szczesniak {
 
         void Update() {
 
+            //print(currentMoveState);
+
             switch (currentMoveState) {
                 case MoveState.Regular:
 
@@ -42,20 +58,36 @@ namespace Szczesniak {
                     // transitions to other states:
                     if (Input.GetButton("Fire3")) currentMoveState = MoveState.Sprinting;
                     if (Input.GetButtonDown("Fire1")) currentMoveState = MoveState.Sneaking;
-                    if (Input.GetButtonDown("Fire2")) currentMoveState = MoveState.Dashing;
+                    if (Input.GetButtonDown("Fire2")) { // transition to dashing
+                       
+                        currentMoveState = MoveState.Dashing;
+                        float h = Input.GetAxis("Horizontal");
+                        float v = Input.GetAxis("Vertical");
+                        dashDirection = new Vector3(h, 0, v);
+
+                        // clamp the length of dashDir to 1:
+                        if (dashDirection.sqrMagnitude > 1)
+                            dashDirection.Normalize();
+                    }
 
                     break;
                 case MoveState.Dashing:
 
                     // do behaviour for this state:
-                    DashMove(25);
+                    //DashMove(25);
+                    DashThePlayer();
+
+                    dashTimer -= Time.deltaTime;
+
                     // transitions to other states:
 
-                    dashTime -= Time.deltaTime;
-                    if (dashTime <= 0) {
-                        currentMoveState = MoveState.Regular;
-                        dashTime = .3f;
-                    }
+                    if (dashTimer <= 0) currentMoveState = MoveState.Regular;
+
+                    //dashTime -= Time.deltaTime;
+                    //if (dashTime <= 0) {
+                    //    currentMoveState = MoveState.Regular;
+                    //    dashTime = .3f;
+                    //}
 
                     break;
                 case MoveState.Sprinting:
@@ -84,6 +116,10 @@ namespace Szczesniak {
 
         }
 
+        private void DashThePlayer() {
+            pawn.Move(dashDirection * Time.deltaTime * 100);
+        }
+
         private void MoveThePlayer(float mult = 1) {
             // Getting player input values of 0 to 1
             float h = Input.GetAxis("Horizontal");
@@ -98,8 +134,8 @@ namespace Szczesniak {
             pawn.Move(move * Time.deltaTime * playerSpeed * mult);
         }
 
-        private void DashMove(float dashSpeed = 1f) {
-            pawn.Move(transform.forward * dashSpeed * Time.deltaTime);
-        }
+        //private void DashMove(float dashSpeed = 1f) {
+        //    pawn.Move(transform.forward * dashSpeed * Time.deltaTime);
+        //}
     }
 }
