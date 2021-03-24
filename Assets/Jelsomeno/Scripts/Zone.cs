5e2839c899228b078e654741a2d1923af9624fea
Rocco <roccojelso77@gmail.com>
2021-03-24 15:53:06 -0400

    End of Week 11 Lab
    -added a foreground raycast script as the last thing for the day

---
+++ Assets/Jelsomeno/Scripts/Zone.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Jelsomeno
+{
+    public class Zone : Outbreak.Zone
+    {
+
+        new static public ZoneInfo info = new ZoneInfo()
+        {
+            zoneName = "Tank Buster",
+            creator = "Rocco Jelsomeno",
+            sceneFile = "Jelsomeno"
+        };
+
+    }
+
+}
