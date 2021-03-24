10ee87a7f59e770d9189ceba12ff034ae17446dd
Jordan Smith <smitj306@ferris.edu>
2021-03-24 15:53:36 -0400

    Added zone info, hidey wall
    added zone info script and added script to hide wall when player is near/behind it

---
+++ Assets/_JSmith/Scripts/Zone.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _JSmith
+{
+    public class Zone : Outbreak.Zone
+    {
+
+        new static public ZoneInfo info = new ZoneInfo()
+        {
+            zoneName = "Zargul Battle",
+            creator = "Jordan Smith",
+            sceneFile = "_JSmithLevel"
+
+        };
+
+
+
+    }
+}
