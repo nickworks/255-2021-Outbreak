3a634281af0c0df25ab5a717da7865a1a1230b73
Nick Pattison <pattison.nick@gmail.com>
2021-03-24 13:25:42 -0400

    Fixing some namespace conflicts

--- Assets/ASmith/Scripts/Projectile.cs
+++ Assets/ASmith/Scripts/Projectile.cs
@@ -2,43 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Projectile : MonoBehaviour
-{
-    /// <summary>
-    /// Current speed and direction the player is moving
-    /// </summary>
-    private Vector3 velocity;
+namespace ASmith {
+    public class Projectile : MonoBehaviour {
+        /// <summary>
+        /// Current speed and direction the player is moving
+        /// </summary>
+        private Vector3 velocity;
 
-    /// <summary>
-    /// How long the projectile should live, in seconds
-    /// </summary>
-    private float lifespan = 3;
+        /// <summary>
+        /// How long the projectile should live, in seconds
+        /// </summary>
+        private float lifespan = 3;
 
-    /// <summary>
-    /// How long the projectile has been alive, in seconds
-    /// </summary>
-    private float age = 0;
-    void Start()
-    {
-        
-    }
+        /// <summary>
+        /// How long the projectile has been alive, in seconds
+        /// </summary>
+        private float age = 0;
+        void Start() {
 
-    public void InitBullet(Vector3 vel)
-    {
-        velocity = vel;
-    }
-
-    void Update()
-    {
-        age += Time.deltaTime;
-        if (age > lifespan)
-        {
-            Destroy(gameObject);
         }
 
-        // euler physics integration
-        transform.position += velocity * Time.deltaTime;
+        public void InitBullet(Vector3 vel) {
+            velocity = vel;
+        }
 
-        
+        void Update() {
+            age += Time.deltaTime;
+            if (age > lifespan) {
+                Destroy(gameObject);
+            }
+
+            // euler physics integration
+            transform.position += velocity * Time.deltaTime;
+
+
+        }
     }
 }
--- Assets/Hodgkins/Scripts/Projectile.cs
+++ Assets/Hodgkins/Scripts/Projectile.cs
@@ -2,46 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Projectile : MonoBehaviour
-{
-    /// <summary>
-    /// Current speed (and direction) the player is moving
-    /// </summary>
-    private Vector3 velocity;
+namespace Hodgkins {
+    public class Projectile : MonoBehaviour {
+        /// <summary>
+        /// Current speed (and direction) the player is moving
+        /// </summary>
+        private Vector3 velocity;
 
-    /// <summary>
-    /// How long the projectile should live, in seconds
-    /// </summary>
-    private float lifespan = 3;
+        /// <summary>
+        /// How long the projectile should live, in seconds
+        /// </summary>
+        private float lifespan = 3;
 
-    /// <summary>
-    /// How many seconds this projectile has been alive, in seconds
-    /// </summary>
-    private float age = 0;
+        /// <summary>
+        /// How many seconds this projectile has been alive, in seconds
+        /// </summary>
+        private float age = 0;
 
-    void Start()
-    {
-        
-    }
+        void Start() {
 
-    public void InitBullet(Vector3 vel)
-    {
-        velocity = vel;
-    }
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        age += Time.deltaTime;
-        if(age > lifespan)
-        {
-            Destroy(gameObject);
         }
-        
-        // euler physics integration
-        transform.position += velocity * Time.deltaTime;
+
+        public void InitBullet(Vector3 vel) {
+            velocity = vel;
+        }
 
 
+        // Update is called once per frame
+        void Update() {
+            age += Time.deltaTime;
+            if (age > lifespan) {
+                Destroy(gameObject);
+            }
+
+            // euler physics integration
+            transform.position += velocity * Time.deltaTime;
+
+
+        }
     }
 }
