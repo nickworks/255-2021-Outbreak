8e1d6177fb0acecea08b780e075ce5574a9a5365
Rocco <roccojelso77@gmail.com>
2021-03-31 07:45:12 -0400

    Update 1
    +Added/Created basic assets for the boss and player controller as well as imported some environment assets.
    
    +Starting to get the state machine going for the boss, I have been trying the past few days to set it up correctly but have been struggling.
    
    -Still have a lot of work to do, know what I want to do just having trouble implementing it.

--- Assets/Jelsomeno/Scripts/ForegroundRaycaster.cs
+++ Assets/Jelsomeno/Scripts/ForegroundRaycaster.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
 namespace Jelsomeno
 {
+    
     public class ForegroundRaycaster : MonoBehaviour
     {
 
@@ -54,3 +56,4 @@ namespace Jelsomeno
     }
 
 }
+*/
