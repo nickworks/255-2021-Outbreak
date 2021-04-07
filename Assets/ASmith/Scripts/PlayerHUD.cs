636d7824f277d92154166ebee61803832af7cf44
Antonio <wapasmith@gmail.com>
2021-04-07 13:11:32 -0400

    Fixed Script namespace
    + Added PlayerHUD.cs to my namespace

--- Assets/ASmith/Scripts/PlayerHUD.cs
+++ Assets/ASmith/Scripts/PlayerHUD.cs
@@ -4,35 +4,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerHUD : MonoBehaviour
+namespace ASmith
 {
-    public Image ammo1;
-    public Image ammo2;
-    public Image ammo3;
-    public Image ammo4;
-    public Image ammo5;
-    public Image ammo6;
-    public Image ammo7;
-    public Image ammo8;
-    public Image ammo9;
-    public Image ammo10;
-    float roundsInClip;
+		public class PlayerHUD : MonoBehaviour
+	{
+		public Image ammo1;
+		public Image ammo2;
+		public Image ammo3;
+		public Image ammo4;
+		public Image ammo5;
+		public Image ammo6;
+		public Image ammo7;
+		public Image ammo8;
+		public Image ammo9;
+		public Image ammo10;
+		float roundsInClip;
 
-    void Start()
-    {
-        roundsInClip = 10;
-    }
+		void Start()
+		{
+			roundsInClip = 10;
+		}
 
-    void Update()
-    {
-        RenderAmmoCount();
-    }
+		void Update()
+		{
+			RenderAmmoCount();
+		}
 
-    private void RenderAmmoCount()
-    {
-        //if (PlayerWeapon.roundsInClip == 9)
-        //{
-        //    ammo10.enabled = false;
-        //}
-    }
+		private void RenderAmmoCount()
+		{
+			//if (PlayerWeapon.roundsInClip == 9)
+			//{
+			//    ammo10.enabled = false;
+			//}
+		}
+	}
 }
+
